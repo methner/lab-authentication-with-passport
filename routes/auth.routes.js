07670ae0f568e8bf9e04c192845d7b6c0c6ef933
Ineke Methner <ineke.methner@icloud.com>
2020-11-19 10:18:50 +0100

    iteration 1

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -12,4 +12,48 @@ router.get('/private-page', ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render('passport/private', { user: req.user });
 });
 
+router.get('/signup', (req, res) => {
+  res.render('auth/signup');
+});
+
+
+router.post('/signup', (req, res, next) => {
+  const { username, password } = req.body;
+  if (password.length < 8) {
+    res.render('auth/signup', {
+      message: 'Your password must be 8 characters minimun.'
+    });
+    return;
+  }
+  if (username === '') {
+    res.render('auth/signup', { message: 'Your username cannot be empty' });
+    return;
+  }
+  User.findOne({ username: username }).then(found => {
+    if (found !== null) {
+      res.render('auth/signup', { message: 'This username is already taken' });
+    } else {
+      // we can create a user with the username and password pair
+      const salt = bcrypt.genSaltSync();
+      const hash = bcrypt.hashSync(password, salt);
+
+      User.create({ username: username, password: hash })
+        .then(dbUser => {
+          // login with passport 
+          req.login(dbUser, err => {
+            if (err) {
+              next(err);
+            } else {
+              res.redirect('/');
+            }
+          })
+        })
+        .catch(err => {
+          next(err);
+        });
+    }
+  });
+});
+
+
 module.exports = router;
