eb54511593baca634c5564731343ddd91353146f
Ineke Methner <ineke.methner@icloud.com>
2020-11-19 11:04:41 +0100

    iteration 2

--- app.js
+++ app.js
@@ -31,6 +31,12 @@ app.use(cookieParser());
 
 // Express View engine setup
 
+app.use(require('node-sass-middleware')({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  sourceMap: true
+}));
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
@@ -38,6 +44,101 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
+
+// passport configuration
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    cookie: { maxAge: 24 * 60 * 60 * 1000 },
+    saveUninitialized: false,
+    resave: false,
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60 * 1000
+    })
+  })
+);
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('./models/User.model');
+const bcrypt = require('bcryptjs');
+
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(dbUser => {
+      done(null, dbUser);
+    })
+    .catch(err => {
+      done(err);
+    });
+});
+
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username })
+      .then(found => {
+        if (found === null) {
+          done(null, false, { message: 'Wrong credentials' });
+        } else if (!bcrypt.compareSync(password, found.password)) {
+          done(null, false, { message: 'Wrong credentials' });
+        } else {
+          done(null, found);
+        }
+      })
+      .catch(err => {
+        done(err, false);
+      });
+  })
+);
+
+// const GithubStrategy = require('passport-github').Strategy;
+
+// passport.use(
+//   new GithubStrategy(
+//     {
+//       clientID: process.env.GITHUB_ID,
+//       clientSecret: process.env.GITHUB_SECRET,
+//       callbackURL: 'http://127.0.0.1:3000/auth/github/callback'
+//     },
+//     (accessToken, refreshToken, profile, done) => {
+//       // find a user with profile.id as githubId or create one
+//       console.log(profile);
+//       User.findOne({ githubId: profile.id })
+//         .then(found => {
+//           if (found !== null) {
+//             // user already exists
+//             done(null, found);
+//           } else {
+//             // no user with that github id
+//             console.log(profile);
+//             return User.create({
+//               githubId: profile.id,
+//               name: profile._json.login,
+//               avatar: profile._json.avatar_url
+//             }).then(dbUser => {
+//               done(null, dbUser);
+//             })
+//           }
+//         })
+//         .catch(error => {
+//           done(error);
+//         })
+//     }
+//   )
+// )
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// end of passport
+
 
 // Routes middleware goes here
 const index = require('./routes/index.routes');
